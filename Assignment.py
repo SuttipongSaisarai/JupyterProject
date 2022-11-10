8263a18be44a597566cf52059c9e64ef1fa011d4
SuttipongSaisarai <117917763+SuttipongSaisarai@users.noreply.github.com>
2022-11-10 20:46:30 +0700

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,2 +1,11 @@
-My Jupyter Notebook on IBM Watson Studio
-print("Science")
+#My Jupyter Notebook on IBM Watson Studio
+#Suttipong Saisarai
+#Data Science
+#I am interested in data science because I like data driven improvement 
+#The below should print Data Science
+print("Data Science")
+#What I want to practice are
+#1.English
+#2.Six sigma
+#3.Data science
+#4.Programming
